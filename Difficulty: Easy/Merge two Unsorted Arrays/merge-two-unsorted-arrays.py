92df66f36cbf48bf035ee0db92262b5d659f82a5
Himanshu Kumar <himanshukumar8348@gmail.com>
2026-08-12 23:22:07 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Merge two Unsorted Arrays/merge-two-unsorted-arrays.py
@@ -0,0 +1,26 @@
+class Solution:
+    def sortedMerge(self, a, b):
+        a.sort()
+        b.sort()
+
+        res = []
+        i = 0
+        j = 0
+
+        while i < len(a) and j < len(b):
+            if a[i] <= b[j]:
+                res.append(a[i])
+                i += 1
+            else:
+                res.append(b[j])
+                j += 1
+
+        while i < len(a):
+            res.append(a[i])
+            i += 1
+
+        while j < len(b):
+            res.append(b[j])
+            j += 1
+
+        return res
