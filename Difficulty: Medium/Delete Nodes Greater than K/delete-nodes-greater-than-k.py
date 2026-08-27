e20df224dcc7ff35ebfd0cb35dcd6ac7f5446266
Himanshu Kumar <himanshukumar8348@gmail.com>
2026-08-27 15:30:17 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Delete Nodes Greater than K/delete-nodes-greater-than-k.py
+++ Difficulty: Medium/Delete Nodes Greater than K/delete-nodes-greater-than-k.py
@@ -1,11 +1,18 @@
+'''Tree node structure
+class Node:
+    def __init__(self, val):
+        self.right = None
+        self.data = val
+        self.left = None
+'''
 class Solution:
     def deleteNode(self, root, k):
         if root is None:
             return None
-            
         if root.data >= k:
-             return self.deleteNode(root.left, k)
-             
-        root.left = self.deleteNode(root.left, k)
+            return self.deleteNode(root.left, k)
         root.right = self.deleteNode(root.right, k)
         return root
+
+        
+        
