155d3e8a159458a01466387f66cf7573aa2f68c2
Himanshu Kumar <himanshukumar8348@gmail.com>
2026-09-11 23:12:09 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Insert in a Sorted List/insert-in-a-sorted-list.py
+++ Difficulty: Easy/Insert in a Sorted List/insert-in-a-sorted-list.py
@@ -1,24 +1,26 @@
+'''Definition of a Linked List Node
+class Node:
+    def __init__(self, val):
+        self.data = val
+        self.next = None
+'''
+
 class Solution:
     def sortedInsert(self, head, key):
         newNode = Node(key)
-
-        # Empty list
         if head is None:
             return newNode
-
-        # Insert at beginning
-        if key <= head.data:
+        if key < head.data:
             newNode.next = head
             return newNode
-
-        # Find correct position
         curr = head
-
-        while curr.next is not None and curr.next.data < key:
+        while curr.next and curr.next.data < key:
             curr = curr.next
-
-        # Insert new node
         newNode.next = curr.next
         curr.next = newNode
-
         return head
+       
+        
+        
+        
+        
